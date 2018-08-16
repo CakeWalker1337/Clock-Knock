19a971050960c3c8a132fcb973d63e394572fdf3
Maxim Kovalev <maxim.kovalev@saritasa.com>
2018-08-16 12:32:44 +0700

    Adding base presenter and view. Implementing Moxy.
    Task: #MISC-308

---
+++ app/src/main/java/com/saritasa/clock_knock/base/BasePresenter.java
@@ -0,0 +1,7 @@
+package com.saritasa.clock_knock.base;
+
+import com.arellomobile.mvp.MvpPresenter;
+
+public class BasePresenter<VIEW extends BaseView> extends MvpPresenter<VIEW>{
+
+}
---
+++ app/src/main/java/com/saritasa/clock_knock/base/BaseView.java
@@ -0,0 +1,7 @@
+package com.saritasa.clock_knock.base;
+
+import com.arellomobile.mvp.MvpView;
+
+public interface BaseView extends MvpView{
+
+}
